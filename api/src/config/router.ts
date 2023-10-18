578d04da694076ae2740594f51e2efef41a4389e
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-18 20:47:54 +0300

    Updated Router Configuration:
    This commit updates the router configuration by adding an error handler middleware to improve error handling

--- api/src/config/router.ts
+++ api/src/config/router.ts
@@ -1,7 +1,9 @@
 import { Express } from "express"
 import { userAuthRoute } from "../core/routes/userAuth"
+import errorHandler from "../core/middlewares/errorHandler";
 
 export default function routerConfig(app: Express) {
 
     app.use('/api/user', userAuthRoute);
+    app.use(errorHandler);
 }
