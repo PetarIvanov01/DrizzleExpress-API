db609f197f30cb70da127a5ac8cdeb021da10598
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-18 20:54:22 +0300

    Add user interface definitions

---
+++ api/src/interface/user.interface.ts
@@ -0,0 +1,11 @@
+export interface UserLoginData {
+    email: string,
+    password: string,
+}
+
+export interface UserRegisterData {
+    email: string,
+    password: string,
+    username: string,
+}
+
