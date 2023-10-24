b2ca196e369d34f1f73929564be88699074c18ca
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-24 21:49:49 +0300

    refactor: Update registerService  with improved error handling and user authentication

--- api/src/core/services/userRegister.ts
+++ api/src/core/services/userRegister.ts
@@ -1,21 +1,43 @@
+import bcryptjs from "bcryptjs"
+
+import { usserAcc } from "../../database/schemas/schema_user"
+import { db } from "../../config/database";
 import { UserRegisterData } from "../../interface/user.interface";
-import validateAuth from "../validations/validateRegister";
+import validateAuth from "../validations/validateAuth";
+
+import { createSession } from "../helpers/authUtil";
 
 const regService = async (userData: UserRegisterData) => {
 
     const errors = validateAuth(userData);
-
     try {
-        if (errors.length > 0) {
-            throw errors
-        }
+        if (errors.length > 0) throw errors;
 
-        //TODO implement register logic
+        const { email, password, username } = userData;
+        
+        const user = await db.query.usserAcc.findFirst({
+            where: (user, { eq }) => eq(user.email, email)
+        })
 
-        return
+        if (user) throw { message: 'Email is taken!' };
+
+        const userValues = {
+            email,
+            username,
+            password: await bcryptjs.hash(password, 10)
+        };
+
+        const createdUser = await db.insert(usserAcc).values(userValues).returning({ id: usserAcc.userId });
+        const token = createSession({ username, id: createdUser[0].id.toString() })
+        
+        return { username, token };
 
     } catch (error) {
-        throw error
+
+        throw {
+            message: 'Register request faild',
+            error
+        }
     }
 
 }
