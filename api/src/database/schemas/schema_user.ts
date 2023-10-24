b0906ae227c16dcf58503bc41ec6ffc6e58a2f19
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-24 21:52:31 +0300

    refactor: Restructure database table definitions

--- api/src/database/schemas/schema_user.ts
+++ api/src/database/schemas/schema_user.ts
@@ -1,12 +1,38 @@
-import { serial, varchar, pgTable, timestamp } from "drizzle-orm/pg-core"
+import { serial, varchar, pgTable, integer, primaryKey, timestamp } from "drizzle-orm/pg-core"
 
-export const users = pgTable('users1', {
-    id: serial('id').primaryKey(),
-    username: varchar('name', { length: 150 }).notNull(),
+export const usserAcc = pgTable('user_accounts', {
+    userId: serial('user_id').primaryKey(),
     email: varchar('email', { length: 150 }).notNull(),
+    username: varchar('name', { length: 150 }).notNull(),
     password: varchar('password', { length: 150 }).notNull(),
-    role: varchar('role', { length: 80 }).$type<"admin" | "customer">().default('customer').notNull(),
-    createdAt: timestamp("created_at").notNull(),
-    updatedAt: timestamp("updated_at").notNull()
+    createdAt: timestamp("created_at", { withTimezone: true }).default(new Date()).notNull()
 });
 
+export const game = pgTable('game', {
+    gameId: serial('game_id').primaryKey(),
+    category: varchar('category', { length: 20 }).notNull()
+});
+
+export const level = pgTable('level', {
+    levelId: serial('level_id').primaryKey(),
+    name: varchar('name', { length: 20 }).notNull(),
+    gameId: integer('game_id').references(() => usserAcc.userId),
+});
+
+export const userGame = pgTable('user_game', {
+    userId: integer('user_id').references(() => usserAcc.userId),
+    gameId: integer('game_id').references(() => game.gameId),
+    levelId: integer('level_id').references(() => level.levelId),
+    totalAnswers: integer('total_answers').notNull(),
+    correctAnswers: integer('correct_answers').notNull(),
+}, (table) => {
+    return {
+        pk: primaryKey(table.userId, table.gameId, table.levelId)
+    }
+});
+
+// export const gameSessions = pgTable('game_sessions', {
+//     id: serial('id').primaryKey(),
+//     userId: integer('user_id').references(() => usserAcc.userId),
+//     gameId: integer('game_id').references(() => game.gameId)
+//   });
