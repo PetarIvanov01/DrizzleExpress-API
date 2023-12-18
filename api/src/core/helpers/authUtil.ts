22a96e3ad94feea8eb7fe66a1106d86165354cab
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-18 23:59:17 +0200

    Update

--- api/src/core/helpers/authUtil.ts
+++ api/src/core/helpers/authUtil.ts
@@ -1,18 +1,22 @@
-import jwt, { JwtPayload, Secret } from "jsonwebtoken"
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 
-const secret = process.env.JWT_SECRET as Secret
+const secret = process.env.JWT_SECRET as Secret;
+const refresh_secret = process.env.JWT_SECRET_REFRESH as Secret;
+
 interface payload {
     id: string,
     username: string
-}
+};
 
-export const createSession = (data: payload) => {
+export const generateAccessToken = (data: payload) => {
     const payload: JwtPayload = {
         id: data.id,
         username: data.username
     };
 
-    return jwt.sign(payload, secret, configJWT);
+    return jwt.sign(payload, secret, {
+        expiresIn: "15min"
+    });
 }
 
 export const verifyToken = (token: string) => {
@@ -20,6 +24,18 @@ export const verifyToken = (token: string) => {
     return jwt.verify(token, secret);
 }
 
-const configJWT = {
-    expiresIn: '2d'
+
+let refreshTokens:any = [];
+
+export const generateRefreshToken = (data: payload) => {
+    const payload: JwtPayload = {
+        id: data.id,
+        username: data.username
+    };
+    
+    const refreshToken = jwt.sign(payload, refresh_secret, { expiresIn: "30min" })
+    refreshTokens.push(refreshToken);
+    console.log(refreshTokens);
+    
+    return refreshToken;
 }
