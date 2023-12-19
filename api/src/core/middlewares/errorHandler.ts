c8a973ac1e85b8354aeaa7411d0dca067b95f162
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-19 22:32:47 +0200

    refactor: Enhance errorHandler with more informative error payload
    Update the errorHandler middleware to provide a more detailed error payload. The payload now includes the 'type', 'message', 'stack', and 'errors' properties for better identification and handling of different error scenarios. Additionally, handle specific cases like TokenExpiredError, JsonWebTokenError, and ValidationError with appropriate messages and status codes.

--- api/src/core/middlewares/errorHandler.ts
+++ api/src/core/middlewares/errorHandler.ts
@@ -1,10 +1,34 @@
 import { NextFunction, Request, Response } from "express"
-
+interface ErrorPayload {
+    type: string,
+    message: string,
+    stack?: any,
+    errors?: any
+}
 const errorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
-    console.log(error);
 
+    let status = 500;
 
-    return res.status(400).send(error)
+    let errorPayload: ErrorPayload = {
+        type: error.name,
+        message: error.message ?? 'Unexpected error occur!',
+        stack: error.stack || null
+    }
+    if (error.name === 'TokenExpiredError') {
+        errorPayload.message = 'Token is expired!'
+        status = 400;
+    }
+    else if (error.name === 'JsonWebTokenError') {
+        errorPayload.message = 'Ivalid Token!'
+        status = 400;
+    }
+    else if (error.name === 'ValidationError') {
+        errorPayload.message = 'Validation failed!';
+        errorPayload.errors = error.errors
+        status = 422;
+    }
+
+    return res.status(status).send(errorPayload);
 }
 
 export default errorHandler
