e2f455e8f7e6d76025e61d732ab2b07f5a8d6218
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-19 22:28:44 +0200

    refactor: Enhance router configuration in Express server
    In the routerConfig function, update the route paths to include a version prefix, such as '/api/v1'. Also, introduce a new userTestRouter for catalog-related routes. These changes aim to improve the organization and versioning of the API routes. Additionally, import errorHandler to handle potential errors across the routes.

--- api/src/config/router.ts
+++ api/src/config/router.ts
@@ -1,9 +1,14 @@
 import { Express } from "express"
 import { userAuthRoute } from "../core/routes/userAuth"
+
 import errorHandler from "../core/middlewares/errorHandler";
+import { userTestRouter } from "../core/routes/test";
+
+const START_POINT_VERSION = '/api/v1'
 
 export default function routerConfig(app: Express) {
 
-    app.use('/api/user', userAuthRoute);
+    app.use(`${START_POINT_VERSION}/user`, userAuthRoute);
+    app.use(`${START_POINT_VERSION}/catalog`, userTestRouter);
     app.use(errorHandler);
 }
