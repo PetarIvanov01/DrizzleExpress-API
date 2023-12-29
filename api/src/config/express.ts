49c404fa31414c4876bac8cb60366b4125438b8f
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-29 17:40:41 +0200

    Update: Added access to public directory

--- api/src/config/express.ts
+++ api/src/config/express.ts
@@ -1,13 +1,15 @@
-import { Express, urlencoded } from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import bodyParser from "body-parser"
+import { Express, urlencoded, static as _static } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 export default function expressConfig(app: Express) {
 
-    app.use(cors({ credentials: true }))
+    app.use(cors({ credentials: true }));
     app.use(bodyParser.json());
-    app.use(urlencoded({ extended: true }))
+    app.use(urlencoded({ extended: true }));
+
+    app.use(_static('public'));
 
     app.use(cookieParser());
-}
+};
