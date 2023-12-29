20ce0a0728ca869c6c607ca973fd674dee2b1ad2
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-29 17:41:11 +0200

    Update: Rename the test route to real one

--- api/src/config/router.ts
+++ api/src/config/router.ts
@@ -2,13 +2,13 @@ import { Express } from "express"
 import { userAuthRoute } from "../core/routes/userAuth"
 
 import errorHandler from "../core/middlewares/errorHandler";
-import { userTestRouter } from "../core/routes/test";
+import { catalogRoute } from "../core/routes/catalog";
 
 const START_POINT_VERSION = '/api/v1'
 
 export default function routerConfig(app: Express) {
 
     app.use(`${START_POINT_VERSION}/user`, userAuthRoute);
-    app.use(`${START_POINT_VERSION}/catalog`, userTestRouter);
+    app.use(`${START_POINT_VERSION}/catalog`, catalogRoute);
     app.use(errorHandler);
-}
+};
