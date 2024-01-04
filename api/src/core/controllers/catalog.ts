07c2e518b0c83a7537db1be7586a6e36b9bdec22
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-04 23:33:08 +0200

    Irefactor: Update controllers to use typescript interfaces and handle query parameters

--- api/src/core/controllers/catalog.ts
+++ api/src/core/controllers/catalog.ts
@@ -1,10 +1,10 @@
 import { Response, Request } from "express";
+import { RequestWithQueryData } from "../middlewares/extractQuerry";
+import IFile from "../../typescript/interfaces/multer.interface";
+
 import { getCatalogData, insertCatalogData } from "../services/catalog";
 
-import IFile from "../../interface/multer.interface";
-
 export const insertCatalogController = async (req: Request, res: Response) => {
-
     try {
         const data = req.body;
         const file = req.file as IFile;
@@ -16,19 +16,21 @@ export const insertCatalogController = async (req: Request, res: Response) => {
         });
 
     } catch (error) {
-        res.json({ error });
+        throw error;
     };
 };
 
-export const getCatalogController = async (req: Request, res: Response) => {
+export const getCatalogController = async (req: RequestWithQueryData, res: Response) => {
     try {
-        const result = await getCatalogData();
+        let searchBy = req.searchBy || {};
+
+        const values = await getCatalogData(searchBy);
 
         res.json({
-            result
+            values
         });
 
-    } catch (error) {
-        res.json({ error });
+    } catch (error: unknown) {
+        throw error;
     };
 };
