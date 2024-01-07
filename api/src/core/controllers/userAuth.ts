95378e5cf329836a482cbd0d2e6f6d3f5bfee9ec
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-07 15:54:43 +0200

    refactor: Update controllers to match new user service structure and add getCurrentUser controller

--- api/src/core/controllers/userAuth.ts
+++ api/src/core/controllers/userAuth.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
-import { UserLoginData, UserRegisterData } from "../../interface/user.interface";
+import { UserLoginData, UserRegisterData } from "../../typescript/interfaces/user.interface";
 import wrapController from "../helpers/wrapperTryCatch";
-import loginService from "../services/userLogin";
-import regService from "../services/userRegister";
+
+import { getUserById } from "../services/userService/user.queries";
+import loginService from "../services/userService/userLogin";
+import regService from "../services/userService/userRegister";
 
 export const loginController = wrapController(async (req: Request, res: Response) => {
 
     const { email, password } = req.body;
-
     const extractedData: UserLoginData = {
         email,
         password
@@ -16,21 +17,36 @@ export const loginController = wrapController(async (req: Request, res: Response
     const payload = await loginService(extractedData);
 
     res.status(200)
-        .send({ message: "You are logged", payload });
-})
+        .json({ message: "You are logged", payload });
+});
 
 export const registerController = wrapController(async (req: Request, res: Response) => {
 
-    const { email, password, username } = req.body;
+    const { email, password } = req.body;
+    const otherInfo = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phoneNumber: req.body.phoneNumber
+    };
 
     const extractedData: UserRegisterData = {
         email,
         password,
-        username
+        otherInfo
     };
 
     const payload = await regService(extractedData);
 
-    res.status(201)
-        .send({ message: "You are registered", payload });
-})
+    res.status(200)
+        .json({ message: "You are registered", payload });
+});
+
+export const getCurrentUser = wrapController(async (req: Request, res: Response) => {
+
+    const { userId } = req.params;
+
+    const payload = await getUserById(userId);
+
+    res.status(200).
+        json({ message: "Current user", payload });
+});
