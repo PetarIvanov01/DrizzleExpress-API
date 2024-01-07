e307abacfd546323baba95956365db3e65bc0098
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-07 16:08:27 +0200

    refactor: Enhance user data interfaces for authentication

--- api/src/typescript/interfaces/user.interface.ts
+++ api/src/typescript/interfaces/user.interface.ts
@@ -3,19 +3,25 @@ import { Request } from "express";
 export interface UserLoginData {
     email: string,
     password: string,
-}
+};
+
+export type RegisterOtherInfoT = {
+    firstName: string,
+    lastName: string,
+    phoneNumber: string
+};
 
 export interface UserRegisterData {
     email: string,
     password: string,
-    username: string,
-}
+    otherInfo: RegisterOtherInfoT
+};
 
 export interface AuthToken {
-    id: string,
+    id: number,
     email: string
-}
+};
 
 export interface RequestUserProp extends Request {
     user?: AuthToken
-}
+};
