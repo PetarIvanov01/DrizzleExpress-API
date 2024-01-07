56be49dafa6f860c2c5a2681c9e0bc7e4536df89
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-07 16:07:13 +0200

    refactor: Generalize and improve user authentication data validation

--- api/src/core/validations/validateAuth.ts
+++ api/src/core/validations/validateAuth.ts
@@ -1,32 +1,22 @@
-import { UserLoginData, UserRegisterData } from "../../interface/user.interface"
+import { RegisterOtherInfoT, UserLoginData, UserRegisterData } from "../../typescript/interfaces/user.interface";
 
-const REQUIRED_FIELDS_LOGIN: Array<keyof UserLoginData> = ['email', 'password'];
-const REQUIRED_FIELDS_REGISTER: Array<keyof UserRegisterData> = ['email', 'username', 'password'];
+type ObjectKey<T> = keyof T;
 
-const validateAuth = (userData: UserLoginData | UserRegisterData) => {
-
-    type ObjectKey = keyof typeof userData
-
-    const requiredFields = 'username' in userData ? REQUIRED_FIELDS_REGISTER : REQUIRED_FIELDS_LOGIN;
-
-    const missingAndEmptyFields = requiredFields.flatMap(key => {
-
-        const value = userData[key as ObjectKey];
-
+const validateFields = <T>(data: T, requiredFields: Array<ObjectKey<T>>) => {
+    return requiredFields.flatMap(key => {
+        const value = data[key];
         if (value === undefined) {
             return { field: key, value: 'Field is required' };
         }
-
         if (typeof value === 'string' && value === '') {
             return { field: key, value: 'Field cannot be empty' };
         }
-
         return [];
     });
+};
 
-    return missingAndEmptyFields;
+const REQUIRED_AUTH_FIELDS: Array<ObjectKey<UserLoginData>> = ['email', 'password'];
+export const validateAuth = (userData: UserLoginData) => validateFields(userData, REQUIRED_AUTH_FIELDS);
 
-}
-export default validateAuth
-
-
+const REQUIRED_REGISTER_OTHER_FIELDS: Array<ObjectKey<RegisterOtherInfoT>> = ["firstName", "lastName", "phoneNumber"];
+export const validateRegisterOtherInfo = (otherInfo: RegisterOtherInfoT) => validateFields(otherInfo, REQUIRED_REGISTER_OTHER_FIELDS);
