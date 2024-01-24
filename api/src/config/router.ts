2a68e32e5ae8667724b8e53440730a07f88ef6d4
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-24 22:11:33 +0200

    Update: Added new '/cart' route

--- api/src/config/router.ts
+++ api/src/config/router.ts
@@ -3,6 +3,7 @@ import { Express } from "express"
 import userAuthRoute from "../core/routes/userAuth"
 import catalogRoute from "../core/routes/catalog.route";
 import errorHandler from "../core/middlewares/errorHandler";
+import cartRoute from "../core/routes/cart.route";
 
 const START_POINT_VERSION = '/api/v1';
 
@@ -10,5 +11,6 @@ export default function routerConfig(app: Express) {
 
     app.use(`${START_POINT_VERSION}/user`, userAuthRoute);
     app.use(`${START_POINT_VERSION}/catalog`, catalogRoute);
+    app.use(`${START_POINT_VERSION}/cart`, cartRoute);
     app.use(errorHandler);
 };
