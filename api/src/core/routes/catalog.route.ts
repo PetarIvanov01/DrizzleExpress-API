aaba0432b89632cb372e9dadc2a338e1b7b2c6f2
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-27 17:07:06 +0200

    Revert "Refactor Route Structure:"
    This reverts commit a0732d17a715cd9cd0ff9848f6fe4a80bd5dc529.

--- api/src/core/routes/catalog.route.ts
+++ api/src/core/routes/catalog.route.ts
@@ -1,27 +1,22 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 import {
     deleteItemFromCatalog,
     getCatalogController,
-    getProductByIdController,
-    insertCatalogController,
-} from '../controllers/catalog';
+    insertCatalogController
+} from "../controllers/catalog";
 
-import querryMiddlware from '../middlewares/extractQuerry';
-import authChecker from '../middlewares/authentication';
-import fileExtractionMiddlawere from '../middlewares/fileExtraction';
+import querryMiddlware from "../middlewares/extractQuerry";
+import authChecker from "../middlewares/authentication";
+import fileExtractionMiddlawere from "../middlewares/fileExtraction";
 
 const catalogRoute = Router();
 
-catalogRoute
-    .get('/', querryMiddlware, getCatalogController)
-    .post(
-        '/',
-        authChecker,
-        fileExtractionMiddlawere('image'),
-        insertCatalogController
-    )
-    .delete(`/`, authChecker, deleteItemFromCatalog);
+catalogRoute.post('/', authChecker, fileExtractionMiddlawere('image'), insertCatalogController);
+catalogRoute.get('/',
+    querryMiddlware,
+    getCatalogController);
 
-catalogRoute.get('/:itemId', getProductByIdController);
+catalogRoute.delete(`/`, authChecker, deleteItemFromCatalog);
+
 export default catalogRoute;
