a0732d17a715cd9cd0ff9848f6fe4a80bd5dc529
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-27 17:02:57 +0200

    Refactor Route Structure: Reorganized the structure of catalog routes using method chaining for better readability.
    Added a new route to handle requests for a specific product by its ID.

--- api/src/core/routes/catalog.route.ts
+++ api/src/core/routes/catalog.route.ts
@@ -1,22 +1,27 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 import {
     deleteItemFromCatalog,
     getCatalogController,
-    insertCatalogController
-} from "../controllers/catalog";
+    getProductByIdController,
+    insertCatalogController,
+} from '../controllers/catalog';
 
-import querryMiddlware from "../middlewares/extractQuerry";
-import authChecker from "../middlewares/authentication";
-import fileExtractionMiddlawere from "../middlewares/fileExtraction";
+import querryMiddlware from '../middlewares/extractQuerry';
+import authChecker from '../middlewares/authentication';
+import fileExtractionMiddlawere from '../middlewares/fileExtraction';
 
 const catalogRoute = Router();
 
-catalogRoute.post('/', authChecker, fileExtractionMiddlawere('image'), insertCatalogController);
-catalogRoute.get('/',
-    querryMiddlware,
-    getCatalogController);
+catalogRoute
+    .get('/', querryMiddlware, getCatalogController)
+    .post(
+        '/',
+        authChecker,
+        fileExtractionMiddlawere('image'),
+        insertCatalogController
+    )
+    .delete(`/`, authChecker, deleteItemFromCatalog);
 
-catalogRoute.delete(`/`, authChecker, deleteItemFromCatalog);
-
+catalogRoute.get('/:itemId', getProductByIdController);
 export default catalogRoute;
