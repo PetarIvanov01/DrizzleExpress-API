8fceaf1c8bc6f62a6227c0d2732d83d40f2417df
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-28 12:23:34 +0200

    Moving the errorHandler in the controller folder

---
+++ api/src/core/controllers/errorHandler.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from 'express';
+import { ValidationError } from '../utils/Errors';
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
+interface ErrorPayload {
+    type: string;
+    message: string;
+    stack?: any;
+    errors?: any;
+}
+const errorHandler = (
+    error: any,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    let status = 500;
+
+    let errorPayload: ErrorPayload = {
+        type: error.name,
+        message: error.message ?? 'Unexpected error occur!',
+        stack: error.stack || null,
+    };
+    if (error instanceof TokenExpiredError) {
+        errorPayload.message = 'Token is expired!';
+        status = 400;
+    } else if (error instanceof JsonWebTokenError) {
+        errorPayload.message = 'Ivalid Token!';
+        status = 400;
+    } else if (error instanceof ValidationError) {
+        errorPayload.message = error.message ?? 'Validation failed!';
+        errorPayload.errors = error.errors;
+        status = 422;
+    }
+
+    return res.status(status).send(errorPayload);
+};
+
+export default errorHandler;
