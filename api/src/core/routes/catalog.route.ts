91ee4f4211c0fd737fed31040c3c4b2739df3fe0
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-28 15:38:27 +0200

    update: add isAdmin middleware to the deleteItemFromCatalog to ensure that only admin can delete items

--- api/src/core/routes/catalog.route.ts
+++ api/src/core/routes/catalog.route.ts
@@ -1,22 +1,26 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 import {
     deleteItemFromCatalog,
     getCatalogController,
-    insertCatalogController
-} from "../controllers/catalog";
+    insertCatalogController,
+} from '../controllers/catalog';
 
-import querryMiddlware from "../middlewares/extractQuerry";
-import authChecker from "../middlewares/authentication";
-import fileExtractionMiddlawere from "../middlewares/fileExtraction";
+import querryMiddlware from '../middlewares/extractQuerry';
+import authChecker from '../middlewares/authentication';
+import fileExtractionMiddlawere from '../middlewares/fileExtraction';
+import isAdmin from '../middlewares/isAdmin';
 
 const catalogRoute = Router();
 
-catalogRoute.post('/', authChecker, fileExtractionMiddlawere('image'), insertCatalogController);
-catalogRoute.get('/',
-    querryMiddlware,
-    getCatalogController);
+catalogRoute.post(
+    '/',
+    authChecker,
+    fileExtractionMiddlawere('image'),
+    insertCatalogController
+);
+catalogRoute.get('/', querryMiddlware, getCatalogController);
 
-catalogRoute.delete(`/`, authChecker, deleteItemFromCatalog);
+catalogRoute.delete(`/`, isAdmin, deleteItemFromCatalog);
 
 export default catalogRoute;
