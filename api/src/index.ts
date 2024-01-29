b00ddf8f3d470da5d6af44ea910bb9dcf223e495
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-29 18:47:17 +0200

    unnecessary updates

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,28 +1,25 @@
-import "dotenv/config";
-import "./config/database";
-import express from "express";
-import expressConfig from "./config/express";
-import routerConfig from "./config/router";
-import notFoundController from "./core/controllers/notFoundController";
+import 'dotenv/config';
+import './config/database';
+import express from 'express';
+import expressConfig from './config/express';
+import routerConfig from './config/router';
+import notFoundController from './core/controllers/notFoundController';
 
 main();
 async function main() {
     const app = express();
 
     try {
-
         expressConfig(app);
         routerConfig(app);
 
-        app.get('*', notFoundController as any)
+        app.get('*', notFoundController);
 
         app.listen(process.env.PORT, () => {
             console.log(`Server is running at ${process.env.PORT}`);
         });
-
-    }
-    catch (error) {
-        console.log(error)
+    } catch (error) {
+        console.log(error);
         process.exit(1);
     }
 }
