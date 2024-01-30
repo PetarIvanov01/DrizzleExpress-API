51a212b3cd09500e6a6467ea7f8b453a2e73d97a
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-30 18:02:37 +0200

    feat: integrate express-jwt for JWT authentication

--- api/src/core/helpers/jwt.utils.ts
+++ api/src/core/helpers/jwt.utils.ts
@@ -1,25 +1,25 @@
-import jwt, { Secret } from "jsonwebtoken";
+import jwt, { Secret } from 'jsonwebtoken';
 
 export interface Payload {
-  id: number;
-  email: string;
+    id: number;
+    email: string;
 }
 
-const privateKey = process.env.JWT_SECRET_REFRESH as Secret;
+const privateKey = process.env.JWT_SECRET as Secret;
 
 export function signJWT(payload: Payload, expiresIn: string | number) {
-  return jwt.sign(payload, privateKey, { expiresIn });
+    return jwt.sign(payload, privateKey, { expiresIn, algorithm: 'HS256' });
 }
 
 export function verifyJWT(token: string): Payload {
-  try {
-    const decoded = jwt.verify(token, privateKey) as Payload;
+    try {
+        const decoded = jwt.verify(token, privateKey) as Payload;
 
-    return {
-      email: decoded.email,
-      id: decoded.id,
-    };
-  } catch (error: any) {
-    throw new Error("Not authorozied!");
-  }
+        return {
+            email: decoded.email,
+            id: decoded.id,
+        };
+    } catch (error: any) {
+        throw new Error('Not authorozied!');
+    }
 }
---
+++ api/src/core/middlewares/authJWT.ts
@@ -0,0 +1,33 @@
+import { Request } from 'express';
+import { expressjwt } from 'express-jwt';
+
+export function authJWT() {
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+        throw new Error('Secret not provided!');
+    }
+
+    return expressjwt({
+        getToken: TokenGetter,
+        secret: secret,
+        algorithms: ['HS256'],
+    }).unless({
+        path: [
+            {
+                url: /\/api\/v1\/user(?:\/.*)?/,
+                methods: ['POST'],
+            },
+            { url: '/api/v1/catalog', methods: ['GET'] },
+        ],
+    });
+}
+// TODO :: Check for admin rights
+function TokenGetter(req: Request): string | Promise<string> | undefined {
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.split(' ')[0] === 'Bearer'
+    ) {
+        return req.headers.authorization.split(' ')[1];
+    }
+    return;
+}
