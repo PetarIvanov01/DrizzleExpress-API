68a8c16c264a6c4381c36afb5f03b087cdf5a4d5
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-30 18:02:53 +0200

    Deprecated

--- api/src/core/middlewares/authentication.ts
+++ api/src/core/middlewares/authentication.ts
@@ -1,12 +1,17 @@
-import { Response, NextFunction } from "express"
-import { RequestUserProp } from "../../typescript/interfaces/user.interface";
-import { verifyJWT } from "../helpers/jwt.utils";
+import { Response, NextFunction } from 'express';
+import { RequestUserProp } from '../../typescript/interfaces/user.interface';
+import { verifyJWT } from '../helpers/jwt.utils';
 
-export default function authChecker(req: RequestUserProp, res: Response, next: NextFunction) {
-
+// !Is not used anymore - Check the authJWT file
+export default function authChecker(
+    req: RequestUserProp,
+    res: Response,
+    next: NextFunction
+) {
     try {
-
-        const accessToken = req.headers.authorization?.split('Bearer')[1].trim();
+        const accessToken = req.headers.authorization
+            ?.split('Bearer')[1]
+            .trim();
 
         if (!accessToken) throw new Error('No access token provided!');
 
@@ -14,12 +19,11 @@ export default function authChecker(req: RequestUserProp, res: Response, next: N
 
         req.user = {
             id: payload.id,
-            email: payload.email
-        }
+            email: payload.email,
+        };
 
         return next();
-
     } catch (error) {
-        next(error)
+        next(error);
     }
-};
+}
