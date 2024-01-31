b1f4313669c05eedb385f185fb2d5b6e4a598985
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-31 20:03:05 +0200

    Changed types to uuid

--- api/src/database/schemas/schema_products.ts
+++ api/src/database/schemas/schema_products.ts
@@ -1,22 +1,38 @@
-import { serial, varchar, pgTable, numeric, integer,text } from "drizzle-orm/pg-core"
-import { users } from "./schema_user";
+import {
+    serial,
+    varchar,
+    pgTable,
+    numeric,
+    integer,
+    text,
+    uuid,
+} from 'drizzle-orm/pg-core';
+import { users } from './schema_user';
 
 export const categories = pgTable('categories', {
     category_id: serial('category_id').primaryKey(),
-    type: varchar('type', { length: 50 }).notNull().$type<"cardio" | "machines" | "free-weights">()
+    type: varchar('type', { length: 50 })
+        .notNull()
+        .$type<'cardio' | 'machines' | 'free-weights'>(),
 });
 
 export const products = pgTable('products', {
-    product_id: serial('product_id').primaryKey(),
-    category_id: integer('category_id').notNull().references(() => categories.category_id),
+    product_id: uuid('product_id').primaryKey().defaultRandom(),
+    category_id: integer('category_id')
+        .notNull()
+        .references(() => categories.category_id),
     title: varchar('title', { length: 50 }).notNull(),
-    price: numeric('price', { precision: 5, scale: 2 }).notNull(),
-    description: varchar("description", { length: 150 }).notNull(),
-    image: text("image")
+    price: numeric('price').notNull(),
+    description: text('description').notNull(),
+    image: text('image'),
 });
 
 export const finished_orders = pgTable('finished_orders', {
-    order_id: serial('order_id').primaryKey(),
-    user_id: integer('user_id').notNull().references(() => users.id),
-    product_id: integer('product_id').notNull().references(() => products.product_id)
+    order_id: uuid('order_id').primaryKey().defaultRandom(),
+    user_id: uuid('user_id')
+        .notNull()
+        .references(() => users.id),
+    product_id: uuid('product_id')
+        .notNull()
+        .references(() => products.product_id),
 });
--- api/src/database/schemas/schema_user.ts
+++ api/src/database/schemas/schema_user.ts
@@ -1,21 +1,16 @@
-import { serial, varchar, pgTable, timestamp, integer } from "drizzle-orm/pg-core"
+import { varchar, pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
-    id: serial('id').primaryKey(),
+    id: uuid('id').primaryKey().defaultRandom(),
     email: varchar('email', { length: 150 }).notNull(),
     password: varchar('password', { length: 150 }).notNull(),
-    createdAt: timestamp("created_at", { withTimezone: true }).default(new Date()).notNull()
+    createdAt: timestamp('created_at', { withTimezone: true })
+        .default(new Date())
+        .notNull(),
 });
 
 export const users_info = pgTable('users_info', {
-    user_id: integer('id').references(() => users.id),
+    user_id: uuid('user_id').references(() => users.id),
     full_name: varchar('full_name', { length: 50 }).notNull(),
-    phone_number: varchar('phone_number', { length: 20 })
+    phone_number: varchar('phone_number', { length: 20 }),
 });
-
-// export const session = pgTable('user_session', {
-//     sessionId: serial('sessionId').primaryKey(),
-//     email: varchar('email', { length: 150 }).notNull(),
-//     createdAt: timestamp("created_at", { withTimezone: true }).default(new Date()).notNull(),
-//     valid: boolean('valid').notNull(),
-// });
