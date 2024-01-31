23165dae4a5f37fb11535bef1d8a6d6bc087b3c1
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-31 16:58:08 +0200

    feat(auth): enhance loginService to include token refresh functionality

--- api/src/core/services/userService/userLogin.ts
+++ api/src/core/services/userService/userLogin.ts
@@ -1,52 +1,56 @@
-import { compare } from "bcryptjs";
-import { Payload, signJWT } from "../../helpers/jwt.utils";
-import { UserLoginData } from "../../../typescript/interfaces/user.interface";
-import { validateAuth } from "../../validations/validateAuth";
-import { getUserByEmail } from "./user.queries";
-import { ValidationError } from "../../utils/Errors";
+import { compare } from 'bcryptjs';
+import { Payload, signJWT, signJWT_Refresh } from '../../helpers/jwt.utils';
+import { UserLoginData } from '../../../typescript/interfaces/user.interface';
+import { validateAuth } from '../../validations/validateAuth';
+import { getUserByEmail } from './user.queries';
+import { ValidationError } from '../../utils/Errors';
 
 const loginService = async (userData: UserLoginData) => {
-  const errors = validateAuth(userData);
+    const errors = validateAuth(userData);
 
-  try {
-    if (errors.length > 0) throw errors;
+    try {
+        if (errors.length > 0) throw errors;
 
-    const { email, password } = userData;
+        const { email, password } = userData;
 
-    const user = await getUserByEmail(email);
+        const user = await getUserByEmail(email);
 
-    if (user.id === undefined || user.password === undefined)
-      throw new Error("Email or password are incorrect!");
+        if (user.id === undefined || user.password === undefined)
+            throw new Error('Email or password are incorrect!');
 
-    await verifyPassword(password, user.password);
+        await verifyPassword(password, user.password);
 
-    const payload: Payload = {
-      id: user.id,
-      email: user.email as string,
-    };
+        const payload: Payload = {
+            id: user.id,
+            email: user.email as string,
+        };
 
-    const token = signJWT(payload, "15min");
+        const token = await signJWT(payload);
+        const refreshToken = await signJWT_Refresh(payload);
 
-    return { ...payload, token };
-  } catch (error: any) {
-    throw new ValidationError("Login request faild", {
-      ...error,
-      message: error.message,
-    });
-  }
+        return {
+            payload: { ...payload, token, refreshToken },
+            refreshToken,
+        };
+    } catch (error: any) {
+        throw new ValidationError('Login request faild', {
+            ...error,
+            message: error.message,
+        });
+    }
 };
 export default loginService;
 
 const verifyPassword = async (
-  originalPassword: string,
-  passedPassword: string
+    originalPassword: string,
+    passedPassword: string
 ): Promise<Error | void> => {
-  try {
-    const verifyPassword = await compare(originalPassword, passedPassword);
-    if (verifyPassword === false)
-      throw new Error("Email or password are incorrect!");
-    return;
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const verifyPassword = await compare(originalPassword, passedPassword);
+        if (verifyPassword === false)
+            throw new Error('Email or password are incorrect!');
+        return;
+    } catch (error) {
+        throw error;
+    }
 };
