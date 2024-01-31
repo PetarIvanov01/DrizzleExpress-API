c50fc2af33d54fb07ed4d4e2520d6d51de20ccee
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-31 16:54:15 +0200

    feat(auth): enhance user authentication with token refresh mechanism
    This commit introduces improvements to the user authentication flow, adding a token refresh mechanism for enhanced security and user experience.
    
    Changes made:
    - Modified `loginController` and `registerController` to include a new `refreshToken` in the response.
    - Utilized cookies to store and manage the `refreshToken`.
    - Added a new controller, `refreshTokensController`, to handle token refresh requests.
    - Implemented functions from 'jwt.utils' for signing and verifying JWTs.
    
    The `refreshTokensController` enables clients to obtain new access tokens without re-entering credentials, contributing to a smoother user authentication experience. The new tokens are securely managed using HTTP-only cookies.

--- api/src/core/controllers/userAuth.ts
+++ api/src/core/controllers/userAuth.ts
@@ -8,6 +8,13 @@ import wrapController from '../helpers/wrapperTryCatch';
 import { getUserById } from '../services/userService/user.queries';
 import loginService from '../services/userService/userLogin';
 import regService from '../services/userService/userRegister';
+import {
+    signJWT,
+    signJWT_Refresh,
+    verifyJWT_Refresh,
+} from '../helpers/jwt.utils';
+
+const oneWeekInMilliseconds = 1000 * 60 * 60 * 24 * 7;
 
 export const loginController = wrapController(
     async (req: Request, res: Response) => {
@@ -17,7 +24,12 @@ export const loginController = wrapController(
             password,
         };
 
-        const payload = await loginService(extractedData);
+        const { payload, refreshToken } = await loginService(extractedData);
+
+        res.cookie('jwt', refreshToken, {
+            httpOnly: true,
+            maxAge: oneWeekInMilliseconds,
+        });
 
         res.status(200).json({ message: 'You are logged', payload });
     }
@@ -38,7 +50,12 @@ export const registerController = wrapController(
             otherInfo,
         };
 
-        const payload = await regService(extractedData);
+        const { payload, refreshToken } = await regService(extractedData);
+
+        res.cookie('jwt', refreshToken, {
+            httpOnly: true,
+            maxAge: oneWeekInMilliseconds,
+        });
 
         res.status(200).json({ message: 'You are registered', payload });
     }
@@ -51,5 +68,34 @@ export const getCurrentUser = wrapController(
         const payload = await getUserById(userId);
 
         res.status(200).json(payload);
+    }
+);
+
+export const refreshTokensController = wrapController(
+    async (req: Request, res: Response) => {
+        const refreshToken = req.cookies.jwt;
+        if (!refreshToken) return res.status(401).send('Not authorized');
+
+        try {
+            const payload = verifyJWT_Refresh(refreshToken);
+            const newAccesstoken = await signJWT(payload);
+            const newRefreshToken = await signJWT_Refresh(payload);
+
+            res.cookie('jwt', newRefreshToken, {
+                httpOnly: true,
+                maxAge: oneWeekInMilliseconds,
+            });
+
+            res.status(200).send({
+                ...payload,
+                token: newAccesstoken,
+            });
+        } catch (error) {
+            res.clearCookie('jwt', {
+                httpOnly: true,
+                maxAge: oneWeekInMilliseconds,
+            });
+            throw error;
+        }
     }
 );
