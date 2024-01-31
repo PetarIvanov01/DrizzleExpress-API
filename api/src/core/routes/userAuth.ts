36830b7310b6923ba2d5edbc1c09049b0cca36a2
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-31 16:57:04 +0200

    feat(routes): extend userAuthRoute with token refresh endpoint
    This commit extends the user authentication route (`userAuthRoute`) to include a new endpoint for token refresh.

--- api/src/core/routes/userAuth.ts
+++ api/src/core/routes/userAuth.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
     getCurrentUser,
     loginController,
+    refreshTokensController,
     registerController,
 } from '../controllers/userAuth';
 import isAdmin from '../middlewares/isAdmin';
@@ -13,4 +14,5 @@ userAuthRoute.get('/', isAdmin, getCurrentUser);
 userAuthRoute.post('/sign-in', loginController);
 userAuthRoute.post('/sign-up', registerController);
 
+userAuthRoute.post('/refreshtoken', refreshTokensController);
 export default userAuthRoute;
