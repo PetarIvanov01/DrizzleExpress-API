d9f52f155e268f00079743c2757ba212bc670543
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-31 16:55:40 +0200

    feat(auth): enhance JWT handling with token refresh support
    This commit improves the JWT handling module to support token refresh functionality, enhancing security and user session management.
    
    Changes made:
    - Introduced a new JWT secret, `refreshKey`, for refreshing tokens.
    - Implemented `signJWT_Refresh` function to sign refresh tokens with the `refreshKey`.
    - Added `verifyJWT_Refresh` function to verify and decode refresh tokens.
    - Modified `signJWT` function to include the issuer field for improved token validation.
    
    The new refresh token functionality allows for secure and controlled access token renewal.

--- api/src/core/helpers/jwt.utils.ts
+++ api/src/core/helpers/jwt.utils.ts
@@ -6,9 +6,48 @@ export interface Payload {
 }
 
 const privateKey = process.env.JWT_SECRET as Secret;
+const refreshKey = process.env.JWT_SECRET_REFRESH as Secret;
 
-export function signJWT(payload: Payload, expiresIn: string | number) {
-    return jwt.sign(payload, privateKey, { expiresIn, algorithm: 'HS256' });
+export function signJWT(payload: Payload): Promise<string> {
+    return new Promise((resolve, reject) => {
+        jwt.sign(
+            payload,
+            privateKey,
+            {
+                expiresIn: '1min',
+                algorithm: 'HS256',
+                issuer: JSON.stringify(payload.id),
+            },
+            (err, encoded) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(encoded as string);
+                }
+            }
+        );
+    });
+}
+
+export function signJWT_Refresh(payload: Payload): Promise<string> {
+    return new Promise((resolve, reject) => {
+        jwt.sign(
+            payload,
+            refreshKey,
+            {
+                expiresIn: '24h',
+                algorithm: 'HS256',
+                issuer: JSON.stringify(payload.id),
+            },
+            (err, encoded) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(encoded as string);
+                }
+            }
+        );
+    });
 }
 
 export function verifyJWT(token: string): Payload {
@@ -21,5 +60,18 @@ export function verifyJWT(token: string): Payload {
         };
     } catch (error: any) {
         throw new Error('Not authorozied!');
+    }
+}
+
+export function verifyJWT_Refresh(token: string): Payload {
+    try {
+        const payload = jwt.verify(token, refreshKey) as Payload;
+
+        return {
+            email: payload.email,
+            id: payload.id,
+        };
+    } catch (error: any) {
+        throw error;
     }
 }
