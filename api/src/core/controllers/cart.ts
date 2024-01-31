a4e290eeb506829ab412a11d791abacf223fe62e
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-01-31 19:59:30 +0200

    Update cart controller

--- api/src/core/controllers/cart.ts
+++ api/src/core/controllers/cart.ts
@@ -1,23 +1,18 @@
-import { Request, Response } from "express";
-import { getCartDataService } from "../services/cart";
-import validateItemsStringId from "../validations/validateStringIds";
+import { Request, Response } from 'express';
+import { getCartDataService } from '../services/cart';
 
 export const getCartDataController = async (req: Request, res: Response) => {
     try {
-
         const itemsId = req.body.ids;
 
-        if (itemsId === undefined || validateItemsStringId(itemsId) === false) return res.json([]);
+        if (itemsId === undefined) return res.json([]);
 
         const parsedIDs = JSON.parse(itemsId);
+
         const items = await getCartDataService(parsedIDs);
 
-        res.
-            status(200)
-            .send(items);
-
+        res.status(200).send(items);
     } catch (error: unknown) {
         throw error;
-    };
+    }
 };
-
