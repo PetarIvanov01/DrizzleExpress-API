a5de904a8de9e1c61c84eae6078c03dfdf598294
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-01 16:03:14 +0200

    refactor(auth): update authJWT middleware with additional route configurations
    - Updated the 'unless' clause with more specific route configurations:
      - Added '/api/v1/catalog(?:\/.*)?' to allow GET requests without authentication.
      - Added '/api/v1/refreshtoken' to permit POST requests without authentication.

--- api/src/core/middlewares/authJWT.ts
+++ api/src/core/middlewares/authJWT.ts
@@ -13,22 +13,17 @@ export function authJWT() {
         algorithms: ['HS256'],
     }).unless({
         path: [
-            {
-                url: /\/api\/v1\/user(?:\/.*)?/,
-                methods: ['POST'],
-            },
-            { url: '/api/v1/catalog', methods: ['GET'] },
+            { url: /\/api\/v1\/user(?:\/.*)?/, methods: ['POST'] },
+            { url: /\/api\/v1\/catalog(?:\/.*)?/, methods: ['GET'] },
             { url: '/api/v1/cart', methods: ['POST'] },
+            { url: '/api/v1/refreshtoken', method: 'POST' },
         ],
     });
 }
 // TODO :: Check for admin rights
 function TokenGetter(req: Request): string | Promise<string> | undefined {
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.split(' ')[0] === 'Bearer'
-    ) {
-        return req.headers.authorization.split(' ')[1];
+    if (req.headers.authorization) {
+        return req.headers.authorization;
     }
     return;
 }
