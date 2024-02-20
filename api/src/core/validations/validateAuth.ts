9dac905827de2032604366a7b28b01044caa90ae
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-20 17:44:34 +0200

    The code has been reformatted.  No functional changes have been made.

--- api/src/core/validations/validateAuth.ts
+++ api/src/core/validations/validateAuth.ts
@@ -1,9 +1,12 @@
-import { RegisterOtherInfoT, UserLoginData, UserRegisterData } from "../../typescript/interfaces/user.interface";
+import {
+    RegisterOtherInfoT,
+    UserLoginData,
+} from '../../typescript/interfaces/user.interface';
 
 type ObjectKey<T> = keyof T;
 
 const validateFields = <T>(data: T, requiredFields: Array<ObjectKey<T>>) => {
-    return requiredFields.flatMap(key => {
+    return requiredFields.flatMap((key) => {
         const value = data[key];
         if (value === undefined) {
             return { field: key, value: 'Field is required' };
@@ -15,8 +18,17 @@ const validateFields = <T>(data: T, requiredFields: Array<ObjectKey<T>>) => {
     });
 };
 
-const REQUIRED_AUTH_FIELDS: Array<ObjectKey<UserLoginData>> = ['email', 'password'];
-export const validateAuth = (userData: UserLoginData) => validateFields(userData, REQUIRED_AUTH_FIELDS);
+const REQUIRED_AUTH_FIELDS: Array<ObjectKey<UserLoginData>> = [
+    'email',
+    'password',
+];
+export const validateAuth = (userData: UserLoginData) =>
+    validateFields(userData, REQUIRED_AUTH_FIELDS);
 
-const REQUIRED_REGISTER_OTHER_FIELDS: Array<ObjectKey<RegisterOtherInfoT>> = ["firstName", "lastName", "phoneNumber"];
-export const validateRegisterOtherInfo = (otherInfo: RegisterOtherInfoT) => validateFields(otherInfo, REQUIRED_REGISTER_OTHER_FIELDS);
+const REQUIRED_REGISTER_OTHER_FIELDS: Array<ObjectKey<RegisterOtherInfoT>> = [
+    'firstName',
+    'lastName',
+    'phoneNumber',
+];
+export const validateRegisterOtherInfo = (otherInfo: RegisterOtherInfoT) =>
+    validateFields(otherInfo, REQUIRED_REGISTER_OTHER_FIELDS);
