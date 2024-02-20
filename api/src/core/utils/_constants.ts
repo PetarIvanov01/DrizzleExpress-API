ce76f88e6629ddbe74798ee431388100ea6c3ab3
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-20 17:43:03 +0200

    Added page and perPage to the WORKING_QUERIES constant to support pagination

--- api/src/core/utils/_constants.ts
+++ api/src/core/utils/_constants.ts
@@ -1,3 +1,11 @@
-export const WORKING_QUERIES = ["category", "sort_by", "from", "to"] as const;
-export const CATEGORY_QUERIES = ["cardio", "free-weights", "machines"] as const;
-export const SORT_OPTIONS = ["asc", "desc"] as const;
+export const WORKING_QUERIES = [
+    'category',
+    'sort_by',
+    'from',
+    'to',
+    'page',
+    'perPage',
+] as const;
+
+export const CATEGORY_QUERIES = ['cardio', 'free-weights', 'machines'] as const;
+export const SORT_OPTIONS = ['asc', 'desc'] as const;
