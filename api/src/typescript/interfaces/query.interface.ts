41ecdca603003044ac8f11d83033ed5b6e1ffa3b
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-20 17:45:43 +0200

    The SearchQuery interface has been updated to include page and perPage properties for pagination purposes. Additionally, the Paginate type has been imported for defining the types of these new properties. No other changes have been made.

--- api/src/typescript/interfaces/query.interface.ts
+++ api/src/typescript/interfaces/query.interface.ts
@@ -1,10 +1,17 @@
 import { Request } from 'express';
-import { CategoryQueriesType, PriceType, SortType } from '../types/query.type';
+import {
+    CategoryQueriesType,
+    Paginate,
+    PriceType,
+    SortType,
+} from '../types/query.type';
 
 export interface SearchQuery {
     category?: CategoryQueriesType;
     sort_by?: SortType;
     price?: PriceType;
+    page?: Paginate['page'];
+    perPage?: Paginate['perPage'];
 }
 
 export interface RequestWithQueryData extends Request {
--- api/src/typescript/types/query.type.ts
+++ api/src/typescript/types/query.type.ts
@@ -1,12 +1,17 @@
-import { WORKING_QUERIES } from "../../core/utils/_constants";
+import { WORKING_QUERIES } from '../../core/utils/_constants';
 
-export type CategoryQueriesType = "cardio" | "free-weights" | "machines";
-export type SortType = "asc" | "desc";
+export type CategoryQueriesType = 'cardio' | 'free-weights' | 'machines';
+export type SortType = 'asc' | 'desc';
 export type PriceType = {
-  from?: number;
-  to?: number;
+    from?: number;
+    to?: number;
+};
+
+export type Paginate = {
+    page?: number;
+    perPage?: number;
 };
 
 export type DefinedQueriesType = {
-  [Key in (typeof WORKING_QUERIES)[number]]?: string;
+    [Key in (typeof WORKING_QUERIES)[number]]?: string;
 };
