f6fd3a865a37bf0853a5fdef6e15750f0dd87c89
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-20 17:45:05 +0200

    The code has been updated to improve consistency in naming conventions and file organization.

--- api/src/config/router.ts
+++ api/src/config/router.ts
@@ -1,15 +1,15 @@
-import { Express } from "express";
+import { Express } from 'express';
 
-import userAuthRoute from "../core/routes/userAuth";
-import catalogRoute from "../core/routes/catalog.route";
-import errorHandler from "../core/controllers/errorHandler";
-import cartRoute from "../core/routes/cart.route";
+import authRoute from '../core/routes/authRoute';
+import catalogRoute from '../core/routes/productsRoute';
+import errorHandler from '../core/controllers/errors/errorHandlerController';
+import cartRoute from '../core/routes/cartRoute';
 
-const START_POINT_VERSION = "/api/v1";
+const START_POINT_VERSION = '/api/v1';
 
 export default function routerConfig(app: Express) {
-  app.use(`${START_POINT_VERSION}/user`, userAuthRoute);
-  app.use(`${START_POINT_VERSION}/catalog`, catalogRoute);
-  app.use(`${START_POINT_VERSION}/cart`, cartRoute);
-  app.use(errorHandler);
+    app.use(`${START_POINT_VERSION}/user`, authRoute);
+    app.use(`${START_POINT_VERSION}/catalog`, catalogRoute);
+    app.use(`${START_POINT_VERSION}/cart`, cartRoute);
+    app.use(errorHandler);
 }
