6a2d609f6b4c1dfa02514daadf463db0837587a6
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-26 18:10:43 +0200

    Introduced a Product type to define the structure of a product retrieved from the database. Updated the function signature to accept CartItems as input, representing the cart items stored in cookies.

--- api/src/core/services/cart.ts
+++ api/src/core/services/cart.ts
@@ -1,21 +1,37 @@
 import { db } from '../../config/database';
+import { CartItems } from '../controllers/cart/cartController';
 
-export const getCartDataService = async (ids: string[]) => {
+type Product =
+    | {
+          category_id: number;
+          product_id: string;
+          title: string;
+          price: string;
+          image: string | null;
+          quantity?: number;
+      }
+    | undefined;
+
+export const getCartDataService = async (
+    ids: CartItems
+): Promise<Product[]> => {
     try {
+        const idsKeys = Object.keys(ids);
+
         const data = await Promise.all(
-            ids.map(async (i) => {
-                try {
-                    return await db.query.products.findFirst({
-                        where(products, { eq }) {
-                            return eq(products.product_id, i);
-                        },
-                        columns: {
-                            description: false,
-                        },
-                    });
-                } catch (error) {
-                    throw error;
+            idsKeys.map(async (i) => {
+                const product: Product = await db.query.products.findFirst({
+                    where(products, { eq }) {
+                        return eq(products.product_id, i);
+                    },
+                    columns: {
+                        description: false,
+                    },
+                });
+                if (product) {
+                    product.quantity = ids[i];
                 }
+                return product;
             })
         );
 
