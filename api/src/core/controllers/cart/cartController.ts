08ae0360fec3811a1240dc42c95394d95ebb99b6
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-26 18:09:31 +0200

    Introduced an interface CartItems to define the structure of cart items stored in cookies. Updated the type of parsedItemsObjectIds to CartItems to reflect the expected structure of the parsed cookie data.

--- api/src/core/controllers/cart/cartController.ts
+++ api/src/core/controllers/cart/cartController.ts
@@ -1,17 +1,22 @@
 import { Request, Response } from 'express';
 import { getCartDataService } from '../../services/cart';
 
+export interface CartItems {
+    [key: string]: number;
+}
+
 export const getCartDataController = async (req: Request, res: Response) => {
     try {
         const itemsId = req.cookies['cart-cookie'];
 
         if (typeof itemsId !== 'string') {
-            return res.json([]);
+            return res.status(204).json([]);
         }
 
-        const parsedIDs = JSON.parse(itemsId);
+        const parsedItemsObjectIds = JSON.parse(itemsId);
+        const cartItems = parsedItemsObjectIds.cartItems as CartItems;
 
-        const items = await getCartDataService(parsedIDs);
+        const items = await getCartDataService(cartItems);
 
         res.status(200).send(items);
     } catch (error: unknown) {
