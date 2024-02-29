55648eaddec73e98f2b8c52fb4074ad14c7fb401
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-02-29 23:59:08 +0200

    update: Added users_address table schema
    - Added a new table schema `users_address` to store user addresses with columns for country, city, address, and zip code.

--- api/src/database/schemas/schema_user.ts
+++ api/src/database/schemas/schema_user.ts
@@ -1,4 +1,10 @@
-import { varchar, pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
+import {
+    varchar,
+    integer,
+    pgTable,
+    timestamp,
+    uuid,
+} from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
     id: uuid('id').primaryKey().defaultRandom(),
@@ -13,4 +19,12 @@ export const users_info = pgTable('users_info', {
     user_id: uuid('user_id').references(() => users.id),
     full_name: varchar('full_name', { length: 50 }).notNull(),
     phone_number: varchar('phone_number', { length: 20 }),
+});
+
+export const users_address = pgTable('users_address', {
+    user_id: uuid('user_id').references(() => users.id),
+    country: varchar('country', { length: 100 }),
+    city: varchar('city', { length: 100 }),
+    address: varchar('address', { length: 50 }),
+    zip: integer('zip'),
 });
