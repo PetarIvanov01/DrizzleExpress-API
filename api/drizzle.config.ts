f3ebc2604392e110bf951a990b520148762110e7
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-03-04 21:07:31 +0200

    Add database credentials using environment variables

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,7 +1,11 @@
-import type { Config } from "drizzle-kit"
+import type { Config } from 'drizzle-kit';
+import 'dotenv/config';
 
 export default {
-    schema: "./src/db/schemas/*",
-    out: "./.drizzle/migrations",
-    driver:'pg'
-} satisfies Config
+    schema: './src/database/schemas/*',
+    out: './.drizzle/migrations',
+    driver: 'pg',
+    dbCredentials: {
+        connectionString: `postgres://postgres:password@localhost:5432/${process.env.DB_NAME}`,
+    },
+} satisfies Config;
