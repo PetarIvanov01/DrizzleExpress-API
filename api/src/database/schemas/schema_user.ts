608d6428323f4e61ee05ebfa6c626a3b58948724
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2024-03-04 21:09:36 +0200

    Refactor user schema - Renamed tables and columns for better readability - Adjusted field types for more appropriate use:     - Changed `full_name` to separate `first_name` and `last_name` for better data representation.     - Used `text` for `address` to allow longer.

--- api/src/database/schemas/schema_user.ts
+++ api/src/database/schemas/schema_user.ts
@@ -4,27 +4,36 @@ import {
     pgTable,
     timestamp,
     uuid,
+    serial,
+    text,
 } from 'drizzle-orm/pg-core';
 
-export const users = pgTable('users', {
+export const user = pgTable('user', {
     id: uuid('id').primaryKey().defaultRandom(),
-    email: varchar('email', { length: 150 }).notNull(),
-    password: varchar('password', { length: 150 }).notNull(),
+    email: varchar('email', { length: 255 }).notNull(),
+    password: varchar('password', { length: 255 }).notNull(),
     createdAt: timestamp('created_at', { withTimezone: true })
         .default(new Date())
         .notNull(),
 });
 
-export const users_info = pgTable('users_info', {
-    user_id: uuid('user_id').references(() => users.id),
-    full_name: varchar('full_name', { length: 50 }).notNull(),
-    phone_number: varchar('phone_number', { length: 20 }),
+export const user_profile = pgTable('user_profile', {
+    profile_id: uuid('profile_id').primaryKey().defaultRandom(),
+    user_id: uuid('user_id')
+        .references(() => user.id)
+        .notNull(),
+    first_name: varchar('first_name', { length: 100 }).notNull(),
+    last_name: varchar('last_name', { length: 100 }).notNull(),
+    phone_number: varchar('phone_number', { length: 50 }).notNull(),
 });
 
-export const users_address = pgTable('users_address', {
-    user_id: uuid('user_id').references(() => users.id),
+export const user_address = pgTable('user_address', {
+    id: serial('id').primaryKey(),
+    user_id: uuid('user_id')
+        .references(() => user_profile.profile_id)
+        .notNull(),
     country: varchar('country', { length: 100 }),
     city: varchar('city', { length: 100 }),
-    address: varchar('address', { length: 50 }),
-    zip: integer('zip'),
+    address: text('address'),
+    postcode: integer('postcode'),
 });
