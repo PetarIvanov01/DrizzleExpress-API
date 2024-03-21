afd15e12f8476b3ca1bae6235d34bedf838057bd
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-21 13:44:24 +0200

    feat: Extend user controller to handle address management
    - Added new controller functions to handle user addresses:
      - getUserAddresses: Retrieves all addresses associated with a user.
    
      - addAddressToUser: Adds a new address to the user's profile.
    
      - updateUserAddress: Updates an existing address associated with the user.
    
    - Imported necessary functions from the user service to handle address-related operations.

--- api/src/core/controllers/profile/userController.ts
+++ api/src/core/controllers/profile/userController.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express';
 import wrapController from '../../helpers/wrapperTryCatch';
-import { getUserProfileById } from '../../services/userService/getUser';
+import {
+    getUserProfileById,
+    getAddresses,
+} from '../../services/userService/getUser';
 import { updateUser } from '../../services/userService/userManagement';
+import {
+    createAddress,
+    updateAddress,
+} from '../../services/userService/addressManagement';
 
 export const getCurrentUser = wrapController(
     async (req: Request, res: Response) => {
@@ -25,3 +32,40 @@ export const updateCurrentUser = wrapController(
         res.status(201).json({ message: 'Update user', updatedFields });
     }
 );
+
+export const getUserAddresses = wrapController(async function (
+    req: Request,
+    res: Response
+) {
+    const { userId } = req.params;
+    const { addressId } = req.query as { addressId?: string };
+
+    const payload = await getAddresses(userId, addressId);
+
+    res.status(201).json({ message: 'All user addresses', payload });
+});
+
+export const addAddressToUser = wrapController(async function (
+    req: Request,
+    res: Response
+) {
+    const body = req.body;
+    const { userId } = req.params;
+
+    const payload = await createAddress(userId, body);
+
+    res.status(201).json({ message: 'New address added', payload });
+});
+
+export const updateUserAddress = wrapController(async function (
+    req: Request,
+    res: Response
+) {
+    const body = req.body;
+    const { userId } = req.params;
+    const { addressId } = req.query as { addressId?: string };
+
+    const payload = await updateAddress(userId, body, addressId);
+
+    res.status(201).json({ message: 'New address added', payload });
+});
