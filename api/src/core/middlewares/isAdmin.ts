c5255bb9dec9c1a0ef6f093a75b17bcb2fa67109
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-27 22:16:56 +0200

    Update:  isAdmin middleware with email validation and database query
    In the isAdmin middleware, implement email validation to ensure that the route is only accessible with a valid email. If the email is missing or invalid, an AuthorizationError is thrown. Additionally, integrate a database query to fetch user details based on the provided email. If the user exists and has the 'admin' type, the middleware grants access; otherwise, it throws an AuthorizationError indicating lack of authorization.

--- api/src/core/middlewares/isAdmin.ts
+++ api/src/core/middlewares/isAdmin.ts
@@ -1,18 +1,30 @@
 import { Request, Response, NextFunction } from 'express';
-import { ValidationError } from '../utils/Errors';
-export default function isAdmin(
+import { AuthorizationError } from '../utils/Errors';
+import { db } from '../../config/database';
+export default async function isAdmin(
     req: Request,
     res: Response,
     next: NextFunction
 ) {
     try {
-        const IS_ADMIN = process.env.ADMIN_ID;
-
-        if (IS_ADMIN) {
+        if (!req.body.email) {
+            throw new AuthorizationError(
+                "Can't access this route. Invalid Email!"
+            );
+        }
+        const user = await db.query.user.findFirst({
+            where({ email }, { eq }) {
+                return eq(email, req.body.email);
+            },
+            columns: {
+                type: true,
+            },
+        });
+        if (user?.type === 'admin') {
             return next();
         }
-        throw new ValidationError('Not Authorized', null);
+        throw new AuthorizationError('Not Authorized to perform this call!');
     } catch (error) {
-        throw error;
+        return next(error);
     }
 }
