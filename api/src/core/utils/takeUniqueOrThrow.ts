c199c2caae798b8a1ebe5021f18739011becda2a
PetarIvanov01 <123456789petar45@gmail.com>
2024-03-29 20:08:13 +0200

    func that returns the first value from array

---
+++ api/src/core/utils/takeUniqueOrThrow.ts
@@ -0,0 +1,7 @@
+function takeUniqueOrThrow<T extends any[]>(values: T): T[number] {
+    if (values.length !== 1)
+        throw new Error('Found non unique or inexistent value');
+    return values[0]!;
+}
+
+export default takeUniqueOrThrow;
