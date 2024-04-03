01358ecc8342b768f0ea7ab4eb8c9946f3c46891
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-03 16:53:01 +0300

    refactor registration service - spread all errors into one array

--- api/src/core/services/userService/userRegister.ts
+++ api/src/core/services/userService/userRegister.ts
@@ -9,19 +9,17 @@ import { signJWT, signJWT_Refresh } from '../../helpers/jwt.utils';
 import { createUser } from './userManagement';
 import { ValidationError } from '../../utils/Errors';
 
-//Todo abstract the catch block
-
 const regService = async (userData: UserRegisterData) => {
     const errors = [
-        validateAuth({
+        ...validateAuth({
             email: userData.email,
             password: userData.password,
         }),
-        validateRegisterOtherInfo(userData.otherInfo),
+        ...validateRegisterOtherInfo(userData.otherInfo),
     ];
 
     try {
-        if (errors[0].length > 0 && errors[1].length > 0) throw errors;
+        if (errors.length > 0) throw errors;
 
         const { email, password } = userData;
 
