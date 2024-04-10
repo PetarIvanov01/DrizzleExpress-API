976683c9cdeee4e93da4ad8e994983048b95e745
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-10 17:18:37 +0300

    refactor: - remove unnecessary middleware

--- api/src/core/routes/productsRoute.ts
+++ api/src/core/routes/productsRoute.ts
@@ -7,24 +7,17 @@ import {
     insertCatalogController,
 } from '../controllers/products/productsController';
 
-import { authJWT } from '../middlewares/authJWT';
-
 import querryMiddlware from '../middlewares/extractQuerry';
 import fileExtractionMiddlawere from '../middlewares/fileExtraction';
-import isAdmin from '../middlewares/isAdmin';
+import isAdmin from '../middlewares/guards/isAdmin';
 
 const catalogRoute = Router();
 
 catalogRoute
     .route('/')
     .get(querryMiddlware, getCatalogController)
-    .post(
-        authJWT(),
-        isAdmin,
-        fileExtractionMiddlawere('image'),
-        insertCatalogController
-    )
-    .delete(authJWT(), isAdmin, deleteItemFromCatalog);
+    .post(isAdmin, fileExtractionMiddlawere('image'), insertCatalogController)
+    .delete(isAdmin, deleteItemFromCatalog);
 
 catalogRoute.get('/:productId', getProductByIdController);
 
