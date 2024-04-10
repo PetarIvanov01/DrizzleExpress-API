7c73e2e33c9b03c2b9473fac3f6182d37a96bc2f
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-10 17:19:58 +0300

    refactor: - Imported json from Express to replace bodyParser.json() for parsing JSON requests.

--- api/src/config/express.ts
+++ api/src/config/express.ts
@@ -1,19 +1,19 @@
-import { Express, urlencoded, static as _static } from 'express';
+import { Express, urlencoded, static as _static, json } from 'express';
 
 import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
+
 import bodyTrimmer from '../core/middlewares/trimBody';
+import loggerMiddleware from '../core/middlewares/loggerMiddleware';
 
 import cors from 'cors';
+import { setCorsOptions } from './util';
 
 import serverLogger from '../../loggers/index';
-import loggerMiddleware from '../core/middlewares/loggerMiddleware';
-import { setCorsOptions } from './util';
 
 export default function expressConfig(app: Express) {
     app.use(cors(setCorsOptions(app)));
 
-    app.use(bodyParser.json());
+    app.use(json());
     app.use(urlencoded({ extended: true }));
 
     app.use(bodyTrimmer);
