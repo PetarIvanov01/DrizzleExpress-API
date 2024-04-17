a356f027b6ac4ad18d87e406a3f97419f2a2c6c8
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-17 17:59:02 +0300

    update: Add different options for the cookie, depending on which env is the app

--- api/src/core/controllers/auth/_options.ts
+++ api/src/core/controllers/auth/_options.ts
@@ -2,10 +2,11 @@ import { CookieOptions } from 'express';
 
 const oneWeekInMilliseconds = 1000 * 60 * 60 * 24 * 7;
 export const COOKIE_NAME = 'jwt-refresh';
+
 export const COOKIE_OPTIONS = {
     httpOnly: true,
     maxAge: oneWeekInMilliseconds,
     path: '/api/v1/users/refreshtoken',
-    sameSite: 'strict',
-    secure: true,
+    sameSite: process.env.NODE_ENV?.includes('production') ? 'none' : 'strict',
+    secure: process.env.NODE_ENV?.includes('production') ? true : false,
 } as CookieOptions;
