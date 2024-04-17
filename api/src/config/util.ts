4c348148f3eda3af61523bfe37394ad4b011a279
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-17 13:10:36 +0300

    update cors options

--- api/src/config/util.ts
+++ api/src/config/util.ts
@@ -3,6 +3,7 @@ import { CorsOptions } from 'cors';
 export function setCorsOptions(): CorsOptions {
     const corsOpt: CorsOptions = {
         credentials: true,
+        origin: /^http:\/\/localhost:5173(?:\/.*)?$/,
     };
 
     if (process.env.NODE_ENV?.includes('development')) {
