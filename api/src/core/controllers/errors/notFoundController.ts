5f66d8c16f1da2ea6bfdd7b507e2ddf31b3cb20f
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-18 18:03:43 +0300

    refactor: if the requested URL contains any of the allowed file extensions, it checks if the URL includes 'image'. If it does, it sends the file from the 'public/uploads' directory; otherwise, it sends the file from the 'public' directory.

--- api/src/core/controllers/errors/notFoundController.ts
+++ api/src/core/controllers/errors/notFoundController.ts
@@ -5,12 +5,15 @@ import serverLogger from '../../../../loggers';
 const allowedExtensions = ['.js', '.css', '.png', '.jpg', '.jpeg', '.ico'];
 
 const notFoundController = (req: Request, res: Response) => {
-    serverLogger.info(`Route not found: ${req.method} ${req.url}`);
     if (
         allowedExtensions.filter((ext) => req.url.indexOf(ext) > 0).length > 0
     ) {
+        if (req.url.includes('image')) {
+            res.sendFile(path.resolve(`public/uploads/${req.url}`));
+        }
         res.sendFile(path.resolve(`public/${req.url}`));
     } else {
+        serverLogger.info(`Route not found: ${req.method} ${req.url}`);
         res.sendFile(path.resolve('public/index.html'));
     }
 };
