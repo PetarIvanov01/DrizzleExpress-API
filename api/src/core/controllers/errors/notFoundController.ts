0bfac987a7a7419717f6cc6fab8eac23b625b15b
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-18 19:09:00 +0300

    update

--- api/src/core/controllers/errors/notFoundController.ts
+++ api/src/core/controllers/errors/notFoundController.ts
@@ -2,20 +2,9 @@ import { Request, Response } from 'express';
 import path from 'path';
 import serverLogger from '../../../../loggers';
 
-const allowedExtensions = ['.js', '.css', '.png', '.jpg', '.jpeg', '.ico'];
-
 const notFoundController = (req: Request, res: Response) => {
-    if (
-        allowedExtensions.filter((ext) => req.url.indexOf(ext) > 0).length > 0
-    ) {
-        if (req.url.includes('image')) {
-            res.sendFile(path.resolve(`public/uploads/${req.url}`));
-        }
-        res.sendFile(path.resolve(`public/${req.url}`));
-    } else {
-        serverLogger.info(`Route not found: ${req.method} ${req.url}`);
-        res.sendFile(path.resolve('public/index.html'));
-    }
+    serverLogger.info(`Route not found: ${req.method} ${req.url}`);
+    res.sendFile(path.resolve('public/index.html'));
 };
 
 export default notFoundController;
