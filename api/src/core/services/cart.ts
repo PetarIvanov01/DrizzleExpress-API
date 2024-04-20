3d646d2103848dfaf81c0aace28ec49c407bbeb9
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-20 12:38:51 +0300

    Refactor cart data retrieval logic - Removed unnecessary import of CartItems from cartController in favor of directly importing CartBody from cartBodySchema. - Consolidated and simplified the getCartDataService function into getCartService. - Renamed ids parameter to cartBody for clarity and consistency with the rest of the application. - Updated the function signature and usage to accept cartBody directly instead of extracting cartItems from it. - Removed type union | undefined for the Product type, ensuring a more straightforward and predictable data structure.

--- api/src/core/services/cart.ts
+++ api/src/core/services/cart.ts
@@ -1,55 +1,51 @@
-import { CartItems } from '../controllers/cart/cartController';
 import takeUniqueOrThrow from '../utils/takeUniqueOrThrow';
 
 import { db } from '../../config/database';
 import { categories, products } from '../../database/schemas/schema_products';
 import { eq } from 'drizzle-orm';
+import { CartBody } from '../schemas/cartBodySchema';
 
-type Product =
-    | {
-          category_id: number;
-          product_id: string;
-          title: string;
-          price: number;
-          image: string | null;
-          quantity?: number;
-          type: 'cardio' | 'machines' | 'free-weights';
-      }
-    | undefined;
+type Product = {
+    category_id: number;
+    product_id: string;
+    title: string;
+    price: number;
+    image: string | null;
+    quantity?: number;
+    type: 'cardio' | 'machines' | 'free-weights';
+};
 
-export const getCartDataService = async (
-    ids: CartItems
+export const getCartService = async (
+    cartBody: CartBody
 ): Promise<Product[]> => {
     try {
-        const idsKeys = Object.keys(ids);
+        const cartIds = Object.keys(cartBody.cartItems);
 
-        const data = await Promise.all(
-            idsKeys.map(async (i) => {
-                const product: Product | undefined = await db
-                    .select({
-                        category_id: categories.category_id,
-                        product_id: products.product_id,
-                        title: products.title,
-                        price: products.price,
-                        image: products.image,
-                        type: categories.type,
-                    })
-                    .from(products)
-                    .innerJoin(
-                        categories,
-                        eq(categories.category_id, products.category_id)
-                    )
-                    .where(eq(products.product_id, i))
-                    .then(takeUniqueOrThrow);
+        const cartPromises = cartIds.map(async (i) => {
+            const product: Product = await db
+                .select({
+                    category_id: categories.category_id,
+                    product_id: products.product_id,
+                    title: products.title,
+                    price: products.price,
+                    image: products.image,
+                    type: categories.type,
+                })
+                .from(products)
+                .innerJoin(
+                    categories,
+                    eq(categories.category_id, products.category_id)
+                )
+                .where(eq(products.product_id, i))
+                .then(takeUniqueOrThrow);
 
-                if (product) {
-                    product.quantity = ids[i];
-                }
-                return product;
-            })
-        );
+            if (product) {
+                product.quantity = cartBody.cartItems[i];
+            }
+            return product;
+        });
 
-        return data.filter((el) => el !== undefined);
+        return await Promise.all(cartPromises);
     } catch (error) {
         throw error;
     }
