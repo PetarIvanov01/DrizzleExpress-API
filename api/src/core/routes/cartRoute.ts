34f777e7777a541ae8e1c7ca860061b1c9543b2d
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-20 12:34:08 +0300

    Refactor cart route to include validation middleware and rename controller for consistency

--- api/src/core/routes/cartRoute.ts
+++ api/src/core/routes/cartRoute.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
-import { getCartDataController } from '../controllers/cart/cartController';
+
+import validateBody from '../middlewares/zodBodyValidator';
+import { cartSchema } from '../schemas/cartBodySchema';
+
+import { getCartController } from '../controllers/cart/cartController';
 
 const cartRoute = Router();
-cartRoute.get('/', getCartDataController);
+cartRoute.post('/', validateBody(cartSchema), getCartController);
 
 export default cartRoute;
