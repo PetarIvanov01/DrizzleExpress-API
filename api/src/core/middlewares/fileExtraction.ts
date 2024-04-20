13a1eac5708e1bf358647860db15d649e9e3dcab
PetarIvanov01 <123456789petar45@gmail.com>
2024-04-20 17:46:03 +0300

    feat: - install cloudinary cloud storage
    refactor:
    - Switched file upload handling to use cloud storage
    - Replaced local file storage with cloud storage using Cloudinary.

--- api/src/core/middlewares/fileExtraction.ts
+++ api/src/core/middlewares/fileExtraction.ts
@@ -1,24 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
-import multer, { MulterError } from 'multer';
-import path from 'path';
+import multer from 'multer';
 
-const storage = multer.diskStorage({
-    destination(req, file, callback) {
-        const destinationPath = path.resolve(
-            __dirname,
-            '../../../public/uploads'
-        );
-        callback(null, destinationPath);
-    },
-    filename(req, file, callback) {
-        callback(
-            null,
-            file.fieldname + '_' + Date.now() + path.extname(file.originalname)
-        );
-    },
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.IMAGE_CLOUD_NAME_DB,
+    api_key: process.env.IMAGE_API_KEY,
+    api_secret: process.env.IMAGE_API_SECRET,
 });
 
-const upload = multer({ storage: storage });
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024, //5mb
+    },
+});
 
 export default function fileExtractionMiddlawere(field: string) {
     return (req: Request, res: Response, next: NextFunction) =>
