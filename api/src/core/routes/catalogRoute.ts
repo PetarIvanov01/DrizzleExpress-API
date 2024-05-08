d0aa008caf6b96ae7a277abac47b633a9991c108
PetarIvanov01 <123456789petar45@gmail.com>
2024-05-08 20:38:18 +0300

    rename the file and move getProductByIdController in different file

--- api/src/core/routes/catalogRoute.ts
+++ api/src/core/routes/catalogRoute.ts
@@ -1,15 +1,14 @@
 import { Router } from 'express';
 
-import {
-    deleteItemFromCatalog,
-    getCatalogController,
-    getProductByIdController,
-    insertCatalogController,
-} from '../controllers/products/productsController';
-
 import querryMiddlware from '../middlewares/extractQuerry';
 import fileExtractionMiddlawere from '../middlewares/fileExtraction';
 import isAdmin from '../middlewares/guards/isAdmin';
+
+import {
+    deleteItemFromCatalog,
+    getCatalogController,
+    insertCatalogController,
+} from '../controllers/products/catalogController';
 
 const catalogRoute = Router();
 
@@ -18,7 +17,5 @@ catalogRoute
     .get(querryMiddlware, getCatalogController)
     .post(fileExtractionMiddlawere('image'), isAdmin, insertCatalogController)
     .delete(isAdmin, deleteItemFromCatalog);
-
-catalogRoute.get('/:productId', getProductByIdController);
 
 export default catalogRoute;
