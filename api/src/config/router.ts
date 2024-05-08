06de11e91ee2dd16baedcd205058b8914faf9e3f
PetarIvanov01 <123456789petar45@gmail.com>
2024-05-08 20:26:01 +0300

    feat: Implement "/product" route

--- api/src/config/router.ts
+++ api/src/config/router.ts
@@ -2,15 +2,18 @@ import { Express } from 'express';
 
 import authRoute from '../core/routes/authRoute';
 import userRoute from '../core/routes/userRoute';
-import catalogRoute from '../core/routes/productsRoute';
+import catalogRoute from '../core/routes/catalogRoute';
 import errorHandler from '../core/controllers/errors/errorHandlerController';
 import cartRoute from '../core/routes/cartRoute';
+import productRoute from '../core/routes/productRoute';
 
 const START_POINT_VERSION = '/api/v1';
 
 export default function routerConfig(app: Express) {
     app.use(`${START_POINT_VERSION}/users`, authRoute, userRoute);
     app.use(`${START_POINT_VERSION}/catalog`, catalogRoute);
+    app.use(`${START_POINT_VERSION}/product`, productRoute);
     app.use(`${START_POINT_VERSION}/cart`, cartRoute);
+
     app.use(errorHandler);
 }
