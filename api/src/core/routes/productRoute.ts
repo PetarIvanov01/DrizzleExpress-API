d598671216f739498902fc4b126f0dd46ba0bbd6
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-16 14:23:30 +0300

    refactor: add more routes and middlewares

--- api/src/core/routes/productRoute.ts
+++ api/src/core/routes/productRoute.ts
@@ -5,23 +5,36 @@ import {
     getReviewsController,
     createReviewController,
     deleteReviewController,
+    getReviewController,
 } from '../controllers/products/reviewController';
 
 import { getProductByIdController } from '../controllers/products/productController';
 
 import validateBody from '../middlewares/zodBodyValidator';
-import { isProductIdUUID } from '../middlewares/validateUUID';
+import { isValueParamsUUID } from '../middlewares/validateUUID';
 import { authJWT } from '../middlewares/authJWT';
 
 const productRoute = Router();
 
-productRoute.get('/:productId', isProductIdUUID, getProductByIdController);
+const PARAM_PRODUCT_ID = 'productId';
+const PARAM_REVIEW_ID = 'reviewId';
+
+productRoute.get(
+    '/:productId',
+    isValueParamsUUID(PARAM_PRODUCT_ID),
+    getProductByIdController
+);
 
 productRoute
     .route('/:productId/review')
-    .all(isProductIdUUID)
+    .all(isValueParamsUUID(PARAM_PRODUCT_ID))
     .get(getReviewsController)
-    .post(authJWT(), validateBody(reviewSchema), createReviewController)
+    .post(authJWT(), validateBody(reviewSchema), createReviewController);
+
+productRoute
+    .route('/:productId/review/:reviewId')
+    .all(isValueParamsUUID(PARAM_REVIEW_ID))
+    .get(getReviewController)
     .delete(authJWT(), deleteReviewController);
 
 export default productRoute;
